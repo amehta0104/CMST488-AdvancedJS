514349ff31fc64b63550a69340c4aa08e91f18a5
amehta0104 <45803380+amehta0104@users.noreply.github.com>
2022-07-06 22:36:32 -0400

    final submission for project 2

--- project2/js/exercise1.js
+++ project2/js/exercise1.js
@@ -3,16 +3,28 @@ $(function () {
     $("#add-classes").click(function () {
         $('.selector-examples li:first').addClass('first');
         // Add remaining selectors here
-        $('.selector-examples li:odd').addClass('odd');
-    });
+       
+        $('.selector-examples li:nth-child(odd)').addClass('odd');
+       $('.selector-examples li:nth-child(even)').addClass('even');
+       $('.selector-examples li:last').addClass('last');
+       $('.selector-examples li:nth-child(4),li:nth-child(5)').addClass('highlighter');
+    })
 
     /* Change Text */
     $("#change-language").click(function () {
         // Step 1: Create a new variable called inputValue and set it to the value of the #newLanguage id
+    
+       inputValue = $('#newLanguage').val();
+       console.log(inputValue);
 
         // Step 2: Create a new variable called element and set it to the value of the .currentLanguage class
 
+        let element = $('.currentLanguage').html();
+         console.log(typeof(element));
+
         // Step 4: Using .html (http://api.jquery.com/html/), update element with inputValue
+
+       $(".currentLanguage").html(inputValue);
 
     });
 
@@ -26,10 +38,12 @@ $(function () {
             if ($(this).css('background-color') == boxColor) {
                 // element currently has a color
                 // remove it
+                $(this).css('background-color', '');
             }
             else {
                 // element currently has no background color
                 // add it
+                $(this).css('background-color', boxColor);
             }
         });
     });
@@ -38,12 +52,25 @@ $(function () {
     $("#button_toggle_roundedges").click(function() {
         $(".box").each(function () {
             // Add a toggleClass using $(this) to add the class 'round-edge'
+            $(this).toggleClass('round-edge');
         });
     });
 
     /* Toggles Part 3: Adding new elements */
     $("#button_add_box").click(function() {
+        n = $('#boxes').children().length + 1;
+        console.log(n);
         /* Your code goes here  */
+
+        //create div element each time button is clicked 
+        $("<div/>",{
+        // scalable id naming for each additional box
+        'id': 'box' + n,
+        'class' : 'box outlined',
+
+        } ).appendTo('#boxes');
+       
+         
 
     });
 });
