b048915f730e3a6e2ef7ccb86bce058ef3b131fc
amehta0104 <45803380+amehta0104@users.noreply.github.com>
2022-08-09 19:33:19 -0400

    Update app.js

--- project4/js/app.js
+++ project4/js/app.js
@@ -34,7 +34,7 @@ $('#post-container').on("click", idy, function(){
 $.ajax({
   url:"js/projects.json",
   method: 'GET',
-  data: {"ID":i},
+  data: {"ID":'i'},
   success: function(post){
     console.log(post[i].post_title)
 post_output = 
